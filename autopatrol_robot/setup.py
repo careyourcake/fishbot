79ceda34f69101d0fb99ae91d2321662aaa6b5ab
haoye <2756192935@qq.com>
2026-06-29 15:55:22 +0800

    结束初级导航,语音播报听不清

--- autopatrol_robot/setup.py
+++ autopatrol_robot/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
-
-package_name = 'fishbot_application'
+from glob import glob 
+package_name = 'autopatrol_robot'
 
 setup(
     name=package_name,
@@ -10,13 +10,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+'/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name, ['config/patrol_config.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='haoye',
     maintainer_email='2756192935@qq.com',
     description='TODO: Package description',
-    license='Apache-2.0',
+    license='TODO: License declaration',
     extras_require={
         'test': [
             'pytest',
@@ -24,10 +26,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'init_robot_pose = fishbot_application.init_robot_pose:main',
-            'get_robot_pose = fishbot_application.get_robot_pose:main',
-            'nav_to_pose = fishbot_application.nav_to_pose:main',
-            'waypoint_follower = fishbot_application.waypoint_follower:main',
+            'patrol_node = autopatrol_robot.patrol_node:main',
+            'speaker = autopatrol_robot.speaker:main',
         ],
     },
 )
